6838f862b96bb91bf5e3c35424e9f0bdc7ffac3f
pabasaraRavindaka <hprchandrasekara@gmail.com>
2024-11-26 21:13:53 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MenuDisplayPage from "./Page/Menu";
 import ImageUploadPage from "./Page/Admin";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <Router>
+      <Toaster position="top-right" reverseOrder={false} />
       <Routes>
         <Route path="/upload" element={<ImageUploadPage />} />
         <Route path="/" element={<MenuDisplayPage />} />
--- src/Page/Menu.jsx
+++ src/Page/Menu.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { firestore } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const Menu = () => {
   const navigate = useNavigate();
@@ -9,13 +10,18 @@ const Menu = () => {
   const [showModal, setShowModal] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoad, setLoad] = useState(false);
 
   const handleClick = () => {
     setShowModal(true);
   };
+
   const handleModalSubmit = () => {
     if (email === "admin@gmail.com" && password === "1001") {
       navigate("/upload");
+      toast.success("Login successful!");
+    } else {
+      toast.error("Incorrect email or password.");
     }
   };
 
@@ -23,12 +29,12 @@ const Menu = () => {
     const fetchImages = async () => {
       try {
         const snapshot = await getDocs(collection(firestore, "images"));
-        console.log(snapshot);
         const images = snapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
         setImageList(images);
+        setLoad(true);
       } catch (error) {
         console.error("Error fetching images:", error);
       }
@@ -39,34 +45,58 @@ const Menu = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 pb-9">
-      <div className="">
-        {" "}
-        <header className="flex justify-center w-full p-4 mb-10 bg-black ">
-          <h1 className="text-4xl font-bold text-white">Menu</h1>
-        </header>
-        <div className="container flex flex-col items-center mx-auto  max-h-[80vh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 rounded-lg shadow-lg">
-          {imageList.length ? (
-            imageList.map((image, index) => (
-              <div key={index} className="p-4">
+      <header className="flex justify-center w-full p-4 mb-10 bg-black">
+        <h1 className="text-4xl font-bold text-white">Menu</h1>
+      </header>
+
+      <div className="container flex flex-col items-center mx-auto max-h-[80vh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 rounded-lg shadow-lg">
+        {isLoad ? (
+          imageList.map((image, index) => (
+            <div key={index} className="p-4 h-[80vh]">
+              <div className="relative">
                 <img
                   src={image.url}
                   alt={`Menu Item ${index}`}
                   className="object-cover w-full rounded-lg shadow-lg"
+                  loading="lazy"
+                  onError={(e) => (e.target.src = "/path/to/placeholder.jpg")}
                 />
               </div>
-            ))
-          ) : (
-            <p>No images to display.</p>
-          )}
-        </div>
-        <div className="flex justify-center mt-10 ">
-          <button
-            className="px-6 py-2 text-xl text-white bg-black rounded-lg hover:bg-gray-700"
-            onClick={handleClick}
+            </div>
+          ))
+        ) : (
+          <div
+            role="status"
+            className="h-[100vh] flex justify-center items-center"
           >
-            Login
-          </button>
-        </div>
+            <svg
+              aria-hidden="true"
+              class="w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-black"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span class="sr-only">Loading...</span>
+          </div>
+        )}
+      </div>
+
+      <div className="flex justify-center mt-10">
+        <button
+          className="px-6 py-2 text-xl text-white bg-black rounded-lg hover:bg-gray-700"
+          onClick={handleClick}
+        >
+          Login
+        </button>
       </div>
 
       {showModal && (
